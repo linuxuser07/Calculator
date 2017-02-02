5ae278a7c9030558775adb7eff916a729a0a50c9
Phoenix_mini\Fernylux <insanemonkeyproductions@gmail.com>
2017-02-01 21:31:09 -0600

    Added the listeners to the buttosns so they actually make the calcualtions

--- app/src/main/java/com/example/fernylux/calculator/MainActivity.java
+++ app/src/main/java/com/example/fernylux/calculator/MainActivity.java
@@ -2,12 +2,98 @@ package com.example.fernylux.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    //fields
+    private EditText operand1;
+    private EditText operand2;
+    private Button btnAddition;
+    private Button btnSubtraction;
+    private Button btnDivision;
+    private Button btnMultiplication;
+    private TextView txtResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //initilizing the operands on Create
+        operand1 = (EditText) findViewById(R.id.editOperand1);
+        operand2 = (EditText) findViewById(R.id.editOperand2);
+        btnAddition = (Button) findViewById(R.id.btnAddition);
+        btnSubtraction = (Button) findViewById(R.id.btnSubtraction);
+        btnDivision = (Button) findViewById(R.id.btnDivision);
+        btnMultiplication = (Button) findViewById(R.id.btnMultiplication);
+        txtResult = (TextView) findViewById(R.id.txtResult);
+
+        //listeners for the buttons
+
+        btnAddition.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //Creating the variables to store the values of the operation
+                double oper1 = Double.parseDouble(operand1.getText().toString());
+                double oper2 = Double.parseDouble(operand2.getText().toString());
+
+                //adding the actual value
+                double theResult = oper1 + oper2;
+
+                txtResult.setText(Double.toString(theResult));
+
+            }
+
+        });
+
+        btnSubtraction.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //Creating the variables to store the values of the operation
+                double oper1 = Double.parseDouble(operand1.getText().toString());
+                double oper2 = Double.parseDouble(operand2.getText().toString());
+
+                //adding the actual value
+                double theResult = oper1 - oper2;
+
+                txtResult.setText(Double.toString(theResult));
+
+            }
+
+        });
+        btnDivision.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //Creating the variables to store the values of the operation
+                double oper1 = Double.parseDouble(operand1.getText().toString());
+                double oper2 = Double.parseDouble(operand2.getText().toString());
+
+                //adding the actual value
+                double theResult = oper1 / oper2;
+
+                txtResult.setText(Double.toString(theResult));
+
+            }
+
+        });
+
+        btnMultiplication.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //Creating the variables to store the values of the operation
+                double oper1 = Double.parseDouble(operand1.getText().toString());
+                double oper2 = Double.parseDouble(operand2.getText().toString());
+
+                //adding the actual value
+                double theResult = oper1 * oper2;
+
+                txtResult.setText(Double.toString(theResult));
+
+            }
+
+        });
+
     }
 }
